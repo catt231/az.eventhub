342957dbb1c4015da66c34e8a2a808cbf7353837
Claire Thomson <claire.thomson@arnoldclark.com>
2018-06-20 19:21:48 +0100

    send and receive events

---
+++ az.receiver/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.EventHubs.Processor;
+using Microsoft.Extensions.Configuration;
+
+namespace az.receiver
+{
+    public class Program
+    {
+        private static IConfigurationRoot Configuration { get; set; }
+
+        public static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            Console.WriteLine("Registering EventProcessor...");
+
+            var storageAccountName = $"{Configuration["storage:containername"]}";
+            var storageAccountKey = $"{Configuration["storage:accountkey"]}";
+            var storageConnectionString = string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}", storageAccountName, storageAccountKey);
+
+            var eventProcessorHost = new EventProcessorHost(
+                $"{Configuration["eventhub:name"]}",
+                PartitionReceiver.DefaultConsumerGroupName,
+                $"{Configuration["eventhub:endpoint"]}",
+                storageConnectionString,
+                $"{Configuration["storage:containername"]}");
+
+            await eventProcessorHost.RegisterEventProcessorAsync<Processor>();
+
+            Console.WriteLine("Receiving. Press enter key to stop worker.");
+            Console.ReadLine();
+
+            await eventProcessorHost.UnregisterEventProcessorAsync();
+        }
+    }
+}
---
+++ az.sender/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.EventHubs;
+using Microsoft.Extensions.Configuration;
+
+namespace az.sender
+{
+    public class Program
+    {
+        private static IConfigurationRoot Configuration { get; set; }
+        private static EventHubClient _eventHubClient;
+
+        public static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            var connectionStringBuilder = new EventHubsConnectionStringBuilder($"{Configuration["eventhub:endpoint"]}")
+            {
+                EntityPath = $"{Configuration["eventhub:name"]}"
+            };
+
+            _eventHubClient = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
+
+            await SendMessagesToEventHub();
+
+            await _eventHubClient.CloseAsync();
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+        }
+
+        private static readonly Random Random = new Random();
+        private static ConsoleColor GetRandomConsoleColor()
+        {
+            var consoleColors = Enum.GetValues(typeof(ConsoleColor));
+            return (ConsoleColor)consoleColors.GetValue(Random.Next(consoleColors.Length));
+        }
+
+        private static async Task SendMessagesToEventHub()
+        {
+            while (true)
+            {
+                try
+                {
+                    var colour = GetRandomConsoleColor().ToString();
+                    Console.WriteLine($"{colour} Selected");
+                    await _eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(colour)));
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"{DateTime.Now} > Exception: {exception.Message}");
+                }
+
+                await Task.Delay(10);
+            }
+        }
+    }
+}
